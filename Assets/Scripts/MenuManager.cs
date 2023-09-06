55adb898849026d9f489c75912988ee4d9959191
WillSwift01 <103675097+WillSwift01@users.noreply.github.com>
2023-09-06 18:33:14 -0400

    Added Some UI and a Scene
    Added a Menu Scene, minor script to work the start and exit button. and placed a canvas in the level for buttons (No event connections yet).

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    private SceneManager SceneManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartLevel1()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Options()
+    {
+        //
+    }
+
+    public void ExitGame()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+        Application.Quit();
+    }
+}

df7adea9e29646b2e8b8ebc44aca21b0f45e7c36
WillSwift01 <103675097+WillSwift01@users.noreply.github.com>
2023-09-16 16:02:35 -0400

    Menus are ins scene with flow functionality
    All of the menus as of now (Main, Options, Pause) have full functionality moving in between one another along with fully able to go to level scene from main, back to main from pause, and fully exiting the game from main menu. Control swapping and sound adjusting not implemented since there is no code for that currently to map.

---
+++ Assets/Scripts/ExitLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void exitLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}

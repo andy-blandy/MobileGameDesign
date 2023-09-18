d863ad8599344a59adf7d4095fc74aac4d198e9b
andy-blandy <shsandrewdaniels@gmail.com>
2023-09-18 18:20:32 -0400

    Added (janky) boss battle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,19 +12,26 @@ public class GameManager : MonoBehaviour
     [HideInInspector] public PlayerMovement playerMovementScript;
 
     [Header("Level")]
-    public bool isSpawningLevel = true;
     public GameObject[] levelPieces;
     private Queue<GameObject> spawnedLevelPieces;
     public float groundHeightOfLevel = -1f;
     private Vector3 levelSpawnPos;
 
     [Header("Level Settings")]
+    public bool isSpawningLevel = true;
+    public bool isBossBattle = false;
     public bool respawnPlayerAtCheckpoints = true;
     public bool isCheckingForFailure = false;
     [HideInInspector] public bool playerFailed;
 
+    [Header("Boss Battle")]
+    public GameObject bossPrefab;
+    public GameObject[] bossPlatforms;
+    public GameObject bossInstance;
+
     // This tells us which piece should be spawned
     private int currentPiece = 0;
+    private int currentCheckpoint;
 
     // These are used to keep track of when a new piece should be spawned
     private float positionOnCurrentPiece;
@@ -41,6 +49,14 @@ public class GameManager : MonoBehaviour
         // Instantiate queue
         spawnedLevelPieces = new Queue<GameObject>();
 
+        if (bossPrefab != null )
+        {
+            isBossBattle = true;
+        }
+
+        currentPiece = 0;
+        currentCheckpoint = 0;
+
         // Set-up level
         if (isSpawningLevel)
         {
@@ -54,32 +70,20 @@ public class GameManager : MonoBehaviour
     {
 
         if (player.position.x > 0 && 
-            player.position.x % lengthOfPiece < positionOnCurrentPiece &&
-            isSpawningLevel)
+            player.position.x % lengthOfPiece < positionOnCurrentPiece)
         {
-            SpawnNextPiece();
+            if (isSpawningLevel)
+            {
+                SpawnNextPiece();
+            }
+            else if (isBossBattle)
+            {
+                SpawnBossLevelPiece();
+                bossInstance.GetComponent<Boss>().NextPhase();
+            }
         }
 
         positionOnCurrentPiece = player.position.x % lengthOfPiece;
-    }
-
-    void SpawnNextPiece()
-    {
-        // Find out where to spawn new piece
-        int distanceTravelled = Mathf.FloorToInt(player.position.x / lengthOfPiece) + 1;
-        levelSpawnPos.x = distanceTravelled * lengthOfPiece;
-
-        // Spawn new piece and destroy piece off screen
-        spawnedLevelPieces.Enqueue(Instantiate(levelPieces[currentPiece], levelSpawnPos, Quaternion.identity));
-        Destroy(spawnedLevelPieces.Dequeue());
-
-        // Increment through level pieces
-        // Wrap around to zero if the end of the array has been reached
-        currentPiece++;
-        if (currentPiece == levelPieces.Length)
-        {
-            currentPiece = 0;
-        }
     }
 
     public void BeginLevel()
@@ -93,10 +97,50 @@ public class GameManager : MonoBehaviour
 
         // Spawn two starting pieces
         levelSpawnPos = new Vector3(0f, groundHeightOfLevel, 0f);
-        spawnedLevelPieces.Enqueue(Instantiate(levelPieces[0], levelSpawnPos, Quaternion.identity));
+        spawnedLevelPieces.Enqueue(Instantiate(levelPieces[currentPiece], levelSpawnPos, Quaternion.identity));
 
         levelSpawnPos.x = lengthOfPiece;
-        spawnedLevelPieces.Enqueue(Instantiate(levelPieces[1], levelSpawnPos, Quaternion.identity));
+        spawnedLevelPieces.Enqueue(Instantiate(levelPieces[currentPiece + 1], levelSpawnPos, Quaternion.identity));
+
+        currentPiece = currentPiece + 2;
+    }
+
+    void SpawnNextPiece()
+    {
+        // Find out where to spawn new piece
+        int distanceTravelled = Mathf.FloorToInt(player.position.x / lengthOfPiece) + 1;
+        levelSpawnPos.x = distanceTravelled * lengthOfPiece;
+
+        // Spawn new piece and destroy piece off screen
+        spawnedLevelPieces.Enqueue(Instantiate(levelPieces[currentPiece], levelSpawnPos, Quaternion.identity));
+        Destroy(spawnedLevelPieces.Dequeue());
+
+        // Increment through level pieces
+        currentPiece++;
+        if (currentPiece == levelPieces.Length)
+        {
+            if (!isBossBattle)
+            {
+                // Wrap around to zero if the end of the array has been reached
+                currentPiece = 0;
+            } else
+            {
+                // Begin boss battle phase
+                isSpawningLevel = false;
+                CreateBoss();
+            }
+        }
+
+        // Checkpoints
+        switch (currentPiece)
+        {
+            case 4:
+                currentCheckpoint = 4;
+                break;
+            case 9:
+                currentCheckpoint = 9;
+                break;
+        }
     }
 
     public void SpawnPlayer()
@@ -104,7 +148,8 @@ public class GameManager : MonoBehaviour
         // Respawn player
         if (player != null)
         {
-            Destroy(player.gameObject);
+            player.position = playerSpawn.position;
+            return;
         }
 
         GameObject p = Instantiate(playerPrefab, playerSpawn.position, Quaternion.identity);
@@ -132,7 +177,9 @@ public class GameManager : MonoBehaviour
     {
         if (respawnPlayerAtCheckpoints)
         {
+            currentPiece = currentCheckpoint;
             SpawnPlayer();
+            BeginLevel();
         }
 
         if (isCheckingForFailure)
@@ -141,9 +188,32 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void SpawnBossLevelPiece()
+    {
+        // Find out where to spawn new piece
+        int distanceTravelled = Mathf.FloorToInt(player.position.x / lengthOfPiece) + 1;
+        levelSpawnPos.x = distanceTravelled * lengthOfPiece;
+
+        // Choose a random platform to spawn
+        int randomPiece = Random.Range(0, bossPlatforms.Length - 1);
+
+        // Spawn new piece and destroy piece off screen
+        spawnedLevelPieces.Enqueue(Instantiate(bossPlatforms[randomPiece], levelSpawnPos, Quaternion.identity));
+        Destroy(spawnedLevelPieces.Dequeue());
+    }
+
+    void CreateBoss()
+    {
+        // Find out where to spawn new piece
+        int distanceTravelled = Mathf.FloorToInt(player.position.x / lengthOfPiece) + 1;
+        levelSpawnPos.x = distanceTravelled * lengthOfPiece;
+
+        bossInstance = Instantiate(bossPrefab, levelSpawnPos, Quaternion.identity);
+    }
+
     public void EndLevel()
     {
         // Add code to change scene to the level select screen
-        Debug.Log("RETURN PLAYER TO LEVEL SELECT");
+        SceneManager.LoadScene("LevelSelect");
     }
 }

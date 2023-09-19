3fb8bc506a48320abb9c7e4d796ced33c00fbc2f
WillSwift01 <103675097+WillSwift01@users.noreply.github.com>
2023-09-18 22:39:20 -0400

    Revert "Revert "This Might Ruin It""
    This reverts commit 6a7b1d4e4dd14b59dbf7544d621e40ebd5ac16a9.

--- Assets/Scripts/GUI/LevelProgress.cs
+++ Assets/Scripts/GUI/LevelProgress.cs
@@ -7,14 +7,20 @@ public class LevelProgress : MonoBehaviour
 {
     int score = 0;
     float time = 0f;
+    public int deaths = 0;
+    public bool speed = false;
+
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI levelText;
-    public TextMeshProUGUI progressBar;
+    public TextMeshProUGUI speedText;
+    public TextMeshProUGUI deathText;
 
     // Start is called before the first frame update
     void Start()
     {
         setScoreText();
+        setDeathText();
+        setSpeedText();
     }
 
     // Update is called once per frame
@@ -39,5 +45,24 @@ public class LevelProgress : MonoBehaviour
         scoreText.text = "Count: " + score.ToString();
     }
 
-    
+    public void setDeathText()
+    {
+        deathText.text = "KIA: " + deaths.ToString();
+    }
+
+    public void setSpeedText()
+    {
+        string diff = "";
+        if (speed == true)
+        {
+            diff = "HARD";
+        }
+        else
+        {
+            diff = "EASY";
+        }
+        speedText.text = "Speed: " + diff;
+    }
+
+
 }

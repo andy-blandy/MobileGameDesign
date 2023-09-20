3f21d617a6d9d2aa4dedf28e92e110b5d9eafb8f
WillSwift01 <103675097+WillSwift01@users.noreply.github.com>
2023-09-20 15:03:10 -0400

    Slowly Fixing UI
    UI corrections started. Player status completed.

--- Assets/Scripts/GUI/LevelProgress.cs
+++ Assets/Scripts/GUI/LevelProgress.cs
@@ -7,14 +7,20 @@ public class LevelProgress : MonoBehaviour
 {
     int score = 0;
     float time = 0f;
+    public int deaths = 0;
+    public bool speed = false;
+
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI levelText;
-    public TextMeshProUGUI progressBar;
+    public TextMeshProUGUI speedText;
+    public TextMeshProUGUI deathText;
 
     // Start is called before the first frame update
     void Start()
     {
         setScoreText();
+        setDeathText();
+        setSpeedText();
     }
 
     // Update is called once per frame
@@ -36,8 +42,27 @@ public class LevelProgress : MonoBehaviour
 
     public void setScoreText()
     {
-        scoreText.text = "Count: " + score.ToString();
+        scoreText.text = "Score: " + score.ToString();
     }
 
-    
+    public void setDeathText()
+    {
+        deathText.text = "KIA: " + deaths.ToString();
+    }
+
+    public void setSpeedText()
+    {
+        string diff = "";
+        if (speed == true)
+        {
+            diff = "HARD";
+        }
+        else
+        {
+            diff = "EASY";
+        }
+        speedText.text = "Speed: " + diff;
+    }
+
+
 }
